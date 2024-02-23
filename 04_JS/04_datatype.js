ad96376c7cde7f274a01ee32430c043711a644a6
KIMEUNJI-0129 <nunssubkim@gmail.com>
2024-02-23 16:55:12 +0900

    study: JS_variable,operator,datatype,function

--- 04_JS/04_datatype.js
+++ 04_JS/04_datatype.js
@@ -61,3 +61,208 @@ console.log(obj.def); //obj 안에 def가 없기 때문에, 출력하려고 해�
 // 값이 없다는 것을 의도적으로 명시할 때 사용 -> 개발자가 직접 null 값을 할당
 let empty = null;
 console.log(empty);
+
+console.log("=============배열============="); //console 창에 구분해주려고 넣음!
+
+// 6. array - 배열
+// 순서가 있는 데이터의 묶음
+let fruits = ["apple", "orange", "banana", "grape"];
+let fruits2 = new Array("apple", "orange", "banana", "grape");
+console.log(fruits);
+console.log(fruits2);
+console.log(fruits[2]); // 2번 인덱스 요소를 뽑아 콘솔에 찍음
+
+let arr = [1, "str", false, null, undefined];
+console.log(arr);
+
+// Quiz. 아래의 배열에서 '아이스크림' 콘솔에 찍기
+const letters = [
+  "사", //0 0부터 시작!
+  "스", //1
+  "자", //2
+  "크", //3
+  "진", //4
+  "안", //5
+  "리", //6
+  "이", //7
+  "가", //8
+  "수", //9
+  "림", //10
+  "나", //11
+  "아", //12
+  "으", //13
+  "차", //14
+  "운", //15
+];
+
+console.log(letters[12] + letters[7] + letters[1] + letters[3] + letters[10]);
+console.log("=============배열 응용============="); //console 창에 구분해주려고 넣음!
+
+// 배열 응용
+// 다차원 배열
+// 배열 안에 배열이 있는 형태
+const korean = [
+  ["가", "갸", "거", "겨"], //배열 안의 0번째
+  ["나", "냐", "너", "녀"], //배열 안의 1번째
+  ["다", "댜", "더", "뎌"], //배열 안의 2번째
+];
+
+console.log(korean);
+console.log(korean[0]);
+console.log(korean[1]);
+console.log(korean[2]);
+
+//'냐' 를 콘솔에 찍기
+console.log(korean[1][1]); //1번째 배열의 1번째 요소
+
+// Quiz. 아래의 배열에서 '아이스크림' 콘솔에 찍기
+const letters2 = [
+  ["사", "스", "자", "크"],
+  ["진", "안", "리", "이"],
+  ["가", "수", "림", "나"],
+  ["아", "으", "차", "운"],
+];
+
+console.log(
+  letters2[3][0] +
+    letters2[1][3] +
+    letters2[0][1] +
+    letters2[0][3] +
+    letters2[2][2]
+);
+
+console.log("=============객체============="); //console 창에 구분해주려고 넣음!
+
+// 7. object - 객체
+// 배열은 순서가 있는 반면에 객체는 (키: 값) 형태로 작성
+// { key1: value1, key2: value2, ...}
+let cat = {
+  name: "장화",
+  age: 13,
+  isCute: true,
+  mew: function () {
+    return "냐옹";
+  },
+};
+
+// 점 표기법
+console.log(cat.name); //장화
+console.log(cat.age); //13
+console.log(cat.mew); //함수를 출력
+
+// 대괄호 사용
+console.log(cat["name"]); //문자에 따옴표 붙이는거 잊지 않기!
+console.log(cat["isCute"]);
+
+//변경과 추가 가능
+cat.like = "layla";
+console.log(cat);
+cat.name = "홍련";
+console.log(cat);
+
+/*
+  JS의 자료형 -> 데이터 타입
+  - 기본형 (Primitive)
+    - String
+    - Number
+    - Boolean
+    - Null
+    - Undefined
+  - 객체(Objict)
+    - array (배열)
+    - object (객체) -> (키: 값)을 쌍으로 가지고 중괄호{} 안에 감싸여 작성된 것
+  */
+
+// JS의 이상한 자동 형변환
+// '+' 만 문자열이 그대로 문자열로 취급되어서 '문자의 나열'로 나오고 나머지는 다 문자가 숫자로 변환되어 계산되어 나온다.
+let a = "3";
+let b = 2;
+let c = "10";
+
+console.log(a + c); // 문자열은 단순히 더해서 나온다. '310'
+console.log(c - a); // 7 -> 숫자가 됨
+console.log(a * c); // 30 -> 숫자가 됨
+console.log(c / a); // 3.3333... -> 숫자가 됨
+
+// 숫자 + 문자
+console.log(b + a); // '23' -> 숫자가 문자열로 변환되어 문자의 나열로 나온다.
+console.log(b - a); // -1 -> 문자가 숫자로 변환되어 계산됨
+
+// 실습
+let introduce = {
+  name: "kimeunji",
+  interest: ["music", "movie", "coffee"],
+  hobby: [
+    "playing games",
+    "singing",
+    "eating delicous food",
+    "cooking",
+    "daincing",
+    "exercising",
+  ],
+  isLeader: true,
+  MBTI: "enfj",
+};
+console.log(introduce);
+
+// 자료형 확인
+// typeof
+// 피연산자의 데이터 타입을 문자열로 변환
+console.log("=============typeof============="); //console 창에 구분해주려고 넣음!
+console.log(typeof "문자열"); // String
+console.log(typeof 234); // Number
+console.log(typeof {}); // Object
+console.log(typeof []); // Object
+console.log(typeof NaN); // NaN: Not A Number -> Number // 숫자 아닌 것을 숫자 취급할 때 사용. 기본이 숫자여야 하기 때문에 숫자로 나온다.
+let und;
+console.log(typeof und); //undefined
+
+console.log("=============형 변환============="); //console 창에 구분해주려고 넣음!
+// prompt("값을 입력해주세요.");
+
+// 형 변환
+// 1. ? -> 문자로 변경
+let str1 = true;
+let str2 = 123;
+let str3 = null;
+console.log(String(str1)); // 'true'
+console.log(typeof String(str1)); // string
+console.log(String(str2)); // '123'
+console.log(String(str3)); // 'null'
+console.log(str1.toString()); // 'true'
+//typeof 로 확인해보면 모두 string이 나온다! = 다 숫자 타입으로 형 변환이 된 것.
+
+// 2. ? -> 숫자
+let n1 = true;
+let n2 = false;
+let n3 = "12345";
+let n4 = "123.9";
+console.log(Number(n1)); //1 -> true
+console.log(Number(n2)); //0 -> false
+console.log(typeof Number(n1)); // number로 바뀐 것이 확인이 되었다!
+console.log(Number(n3)); // 12345
+console.log(Number(n4)); // 123.9
+
+//parseInt를 사용해서 숫자로 변환
+console.log(parseInt(n3)); // 12345
+console.log(parseInt(n4)); // 123 -> 정수로 변환시키는 것이기 때문에, 소수 점 버린다.
+console.log(parseFloat(n4)); // 123.9 -> 소수 점 안 버린다.
+
+// 실습 - 수학점수와 영어점수를 더해서 평균값을 정확한 값으로 나오게 하기.
+let mathScore = "77";
+let engScore = "88";
+let avgScore = (Number(mathScore) + Number(engScore)) / 2;
+console.log(avgScore);
+
+// prompt 사용 (사용자에게 값을 입력받아 활용하는 예시)
+//사용자에게 입력받은 값은 문자열로 저장되기 때문에, Number로 강제 형 변환(숫자형)을 시켜 점수들을 더하고 나누는 수식을 통해 값을 구해줌.
+let mathScore2 = Number(prompt("수학 점수를 입력하세요."));
+let engScore2 = Number(prompt("영어 점수를 입력하세요."));
+
+let avgScore2 = (mathScore2 + engScore2) / 2;
+console.log(avgScore2);
+
+//mathScore2 가 제대로 변했는지 확인해보기.
+console.log("mathScore2", mathScore2, typeof mathScore2);
+
+alert(`당신의 평균 점수는 ${avgScore2}입니다!`); //백틱 이용해서 템플릿 리터럴로 점수를 알려줌.
---
+++ 04_JS/05_function.js
@@ -0,0 +1,93 @@
+console.log('connected!');
+
+/* 
+  함수
+  - 특정 작업을 수행하기 위해 독립적으로 설계된 코드 집합
+  - 함수 정의 -> 함수 호출
+  - 함수 정의 / 선언 구조
+    - keyword: function
+    - name: 함수 이름 (camelCase로 많이 작성)
+    - 매개변수(=인자, parameter, arguments): 함수 선언 시 매개변수로 받을 것
+    - body(=scope): 함수 내부 코드
+  - 함수 정의 / 선언 방식
+    - 함수 선언문 (명시적 함수 선언)
+      - function 키워드 사용
+      - ex. function name() {}
+    - 함수 표현식
+      - 함수 이름 작성 X -> 변수를 만들어 함수를 저장
+      - 참고) JS에서는 함수가 값이기 때문에 가능한 방식
+      - ex. const funcName = function() {}
+      - 화살표 함수
+        - function 키워드 사용하지 않고, 화살표로 작성
+        - ex. const funcName = () => {}
+    (참고) 함수 선언문 vs 함수 표현식
+      - 함수 선언문 : function funcName() {} 형태로 작성 / 함수 선언 전 호출 가능
+      - 함수 표현식 : const funcName = function() {} / 함수 선언 전 호출 불가
+*/
+
+// 함수 정의 전에 불러 올 수 있나?
+helloWorld1(); // 명시적 함수 -> 가능
+// helloWorld2(); // 표현식 함수 -> 불가능
+
+// 함수 정의
+// 1. 명시적 함수 선언 -> 함수 선언문
+function helloWorld1() {
+  console.log('HelloWorld!1');
+}
+// 함수 호출
+helloWorld1();
+
+// 2-1. 함수 표현식
+const helloWorld2 = function () {
+  console.log('HelloWorld!2');
+};
+// 함수 호출
+helloWorld2();
+
+// 2-2. 화살표 함수
+const helloWorld3 = () => {
+  console.log('helloWorld!3');
+};
+// 함수 호출
+helloWorld3();
+
+// return 사용
+/* 
+  return: 반환 값 -> 함수 내부 코드의 "최종 결과 값"
+  console.log()로 콘솔을 찍는 것에서 그치지 않고,
+  함수 내부 코드의 최종 결과값을 저장하고 보관하기 위한 키워드
+  - 함수 블럭 안에서 return 키워드를 만나면 함수 실행 중단
+*/
+
+// 인자없이 return 값만 가지고 있는 함수
+function onePlusOne() {
+  return 1 + 1;
+}
+
+// onePlusOne(); // -> 2 적은 것과 동일 // 이전 것들은 이미 함수 안에서 console.log를 넣어서 선언해주었기 때문에, 쓸 필요가 없었음!
+console.log(onePlusOne()); // -> console.log(2) -> 콘솔 창에 2 찍음
+
+function numPlusOne(num) {
+  return num + 1;
+}
+console.log(numPlusOne(5)); // num -> 5 -> 5 + 1 -> 6을 반환 -> console.log(6)
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+console.log(sum(5, 20)); // num1 -> 5 , num2 -> 20, 25를 반환
+
+// 함수의 반환값을 변수에 저장해서 사용
+const result = sum(5, 20);
+console.log(result);
+// 굳이 이렇게 인자를 받아서 사용하는 이유는? -> 여러번 사용이 가능하기 때문이다!
+const result2 = sum(100, 11);
+console.log(result2);
+
+console.log('===============================');
+
+function hello(name) {
+  alert(`Hello! ${name}`);
+}
+const userName = 'lucy';
+hello(userName);
